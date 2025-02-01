120bd8a0a98fbc83592857bd86184d4a39d4e368
Antonio Barbosa <antonio.barbosa@digitalcollege.internal>
2025-02-01 12:01:55 -0300

    a

---
+++ fs2024/back/aula02node/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './layouts/Header'
+import Main from './layouts/Main'
+import Footer from './layouts/Footer'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
